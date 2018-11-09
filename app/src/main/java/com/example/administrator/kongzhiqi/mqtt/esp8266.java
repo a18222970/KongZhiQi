8ab7159e76cac4dc0327ab299aad098b58a48c12
a18222970 <18222970@qq.com>
2018-11-09 11:16:32 +0800

    esp8266

---
+++ app/src/main/java/com/example/administrator/kongzhiqi/mqtt/esp8266.java
@@ -0,0 +1,42 @@
+package com.example.administrator.kongzhiqi.mqtt;
+
+/**
+ * Created by Administrator on 2018/11/9/0009.
+ */
+
+public class esp8266 {
+    /**
+     * bianhao : 0001
+     * kg_bianhao : 01
+     * dongzuo : qidong
+     */
+
+    private String bianhao;
+    private String kg_bianhao;
+    private String dongzuo;
+
+
+    public String getBianhao() {
+        return bianhao;
+    }
+
+    public void setBianhao(String bianhao) {
+        this.bianhao = bianhao;
+    }
+
+    public String getKg_bianhao() {
+        return kg_bianhao;
+    }
+
+    public void setKg_bianhao(String kg_bianhao) {
+        this.kg_bianhao = kg_bianhao;
+    }
+
+    public String getDongzuo() {
+        return dongzuo;
+    }
+
+    public void setDongzuo(String dongzuo) {
+        this.dongzuo = dongzuo;
+    }
+}
